accd72908b27d3b31f39425c1fc43e07a3491b43
Riadh Mouamnia <riadh.mouamnia@gmail.com>
2023-09-01 20:21:00 +0100

    Completed assignment

--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Comment() {
-  return <div>Naturally, I agree with this article.</div>;
+  return (
+    <>
+      <div>Naturally, I agree with this article.</div>
+      <div>Another comment!</div>
+    </>
+  );
 }
 
 export default Comment;
